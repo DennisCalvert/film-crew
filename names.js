135d93cdcd822a74b8fc2ad7a2acf64a0f8fbb90
Dennis Calvert <Dennis@denniscalvert.net>
2022-04-18 18:55:24 -0500

    parking info

---
+++ names.js
@@ -0,0 +1,52 @@
+const names = `Dennis Calvert
+Lizzie Carmichel
+Tara Merryman
+Amanda Myers
+Caiti Wroclawski
+Ryan Barker
+John Litwiniec
+Sarah Danneman
+Michael Wroclawski
+Alex Williams
+Jospeh Bodkin
+Preston Beck
+Joiya Pams
+Robert Wason
+Willy Williams
+Darrel Nance
+Marcus January Guests
+Michael Shackelford
+Will Devine
+Jaysen Micheal
+David Levenson
+Wes & Rachel Eldridge
+Adam Lofton
+Jeremy Davis
+John Harris
+Amanda Carmichael 
+Charlie Brown Sanders
+Maddy Monson
+Sam Higinbothom
+Mathew Davis 
+Steven Shears
+Chris Gaines
+Jennifer Jeter
+Paul Mathews
+Todd Wiloughby
+Harris Josey
+Nathan Cooke
+Jay Clark
+Wes Brown
+Mandi Coker
+Matt Hunt
+Shane Hopson
+Marcus January
+Dustin Grandchamp
+Madeline Zink
+Eland Anthony
+John Rait`
+  .split("\n")
+  .sort();
+
+console.log(names);
+console.log(names.length);
